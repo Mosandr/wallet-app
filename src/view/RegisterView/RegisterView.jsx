0f521c100eef655516dd444d41f715590c50b230
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-08-23 20:49:49 +0300

    Leha prinimay

--- src/view/RegisterView/RegisterView.jsx
+++ src/view/RegisterView/RegisterView.jsx
@@ -1,10 +1,7 @@
-import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import userSelector from '../../redux/auth/authSelectors';
-import Media from 'react-media';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
-import smallHeadImg from '../../images/frame_register_small.svg';
-import bigHeadImg from '../../images/frame_register_big.svg';
+import headImg from '../../images/frame_register.svg';
 import '@pnotify/core/dist/PNotify.css';
 import styles from './RegisterView.module.css';
 
@@ -14,31 +11,11 @@ function RegisterView() {
   return (
     <div className={styles.registerView}>
       <div className={styles.registerViewHead}>
-        <Media
-          queries={{
-            small: '(min-width: 768px) and (max-width: 1279px)',
-            large: '(min-width: 1280px)',
-          }}
-        >
-          {matches => (
-            <Fragment>
-              {matches.small && (
-                <img
-                  src={smallHeadImg}
-                  className={styles.registerViewImage}
-                  alt="Finance App"
-                />
-              )}
-              {matches.large && (
-                <img
-                  src={bigHeadImg}
-                  className={styles.registerViewImage}
-                  alt="Finance App"
-                />
-              )}
-            </Fragment>
-          )}
-        </Media>
+        <img
+          src={headImg}
+          className={styles.registerViewImage}
+          alt="Finance App"
+        />
         <p className={styles.registerViewTitle}>Finance App</p>
         {error && <p>{error}</p>}
       </div>
