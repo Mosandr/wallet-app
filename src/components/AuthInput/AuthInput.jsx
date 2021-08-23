5a84803c6caf60bb88ae3df0868adee3a6eb86bb
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-08-23 17:30:56 +0300

    Password progress bar line added

--- src/components/AuthInput/AuthInput.jsx
+++ src/components/AuthInput/AuthInput.jsx
@@ -1,17 +1,23 @@
 import { Field } from 'formik';
+import ProgressBar from './ProgressBar/ProgressBar';
 import styles from './AuthInput.module.css';
 
-const AuthInput = ({ name, iconType, placeholder, type }) => {
-  const wrapperClassName = styles.authInputFieldWrap + ' ' + styles[iconType];
+const AuthInput = ({ name, iconType, placeholder, type, progressBar }) => {
+  let wrapperClassName = styles.authInputFieldWrap + ' ' + styles[iconType];
+  if (progressBar)
+    wrapperClassName = wrapperClassName + ' ' + styles.withProgressBar;
   return (
-    <div className={wrapperClassName}>
-      <Field
-        className={styles.authInputField}
-        name={name}
-        placeholder={placeholder}
-        type={type}
-      />
-    </div>
+    <>
+      <div className={wrapperClassName}>
+        <Field
+          className={styles.authInputField}
+          name={name}
+          placeholder={placeholder}
+          type={type}
+        />
+      </div>
+      {progressBar && <ProgressBar />}
+    </>
   );
 };
 
