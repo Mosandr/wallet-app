f82237f739d71911864ff5a11b7a4760e4bd1d88
AlexCanDoIt <alexcandoitt@gmail.com>
2021-08-30 18:26:11 +0300

    Added route in lable

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import userSelectors from '../../redux/auth/authSelectors';
 import logoSmall from '../../images/logo_small.svg';
 import styles from './Header.module.css';
@@ -8,7 +9,13 @@ function Header({ onLogout }) {
 
   return (
     <div className={styles.pageHeader}>
-      <img src={logoSmall} alt="logo" className={styles.logo} />
+      <NavLink
+        exact
+        to="/"
+        activeClassName={styles.active_link}
+      >
+        <img src={logoSmall} alt="logo" className={styles.logo} />
+      </NavLink>
       <div className={styles.headerMenu}>
         <div className={styles.headerMenuName}>{userName}</div>
         <div className={styles.headerMenuLogout} onClick={onLogout}>
